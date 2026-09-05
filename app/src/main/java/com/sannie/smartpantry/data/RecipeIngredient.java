0ae64a7a9fd4bc815afa6a0e5e5a56fb989875fe
SannieMachete <sanniemachete@gmail.com>
2026-09-05 12:05:30 +0200

    Add RecipeIngredient class to represent recipe ingredients

--- app/src/main/java/com/sannie/smartpantry/data/RecipeIngredient.java
+++ app/src/main/java/com/sannie/smartpantry/data/RecipeIngredient.java
@@ -1,1 +1,64 @@
+package com.sannie.smartpantry.data;
 
+/**
+ * Represents one ingredient required by a recipe.
+ */
+public class RecipeIngredient {
+
+    private long id;
+    private long recipeId;
+    private String ingredientName;
+    private double quantity;
+    private String unit;
+
+    public RecipeIngredient() {
+    }
+
+    public RecipeIngredient(long id, long recipeId, String ingredientName, double quantity, String unit) {
+        this.id = id;
+        this.recipeId = recipeId;
+        this.ingredientName = ingredientName;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(long recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
